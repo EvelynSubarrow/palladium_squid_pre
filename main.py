97f8491fcf1134c84c2f3a865900075be120bb98
Evelyn <git@evelyn.moe>
2021-06-28 01:02:22 +0100

    restructure to avoid using read/write as public variables

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from palladium_squid.ssh_tunnelling import carousel_from_file, create_all, SSHTr
 SAFE_ASCII = range(32, 128)
 
 
-class Client:
+class Connection:
     def __init__(self, sock, control=False):
         if type(sock) in [tuple, list]:
             self.socket = sock[0]
@@ -34,6 +34,10 @@ class Client:
         self.write_buffer = b""
 
         self.pair = None
+
+        self.requested_pair = None
+
+        self.closed = False
 
         self.address = self.socket.getpeername()[0]
         if "." in self.address:
@@ -54,17 +58,15 @@ class Client:
 
     def append_write(self, data: bytes):
         self.write_buffer += data
-        write.append(self)
 
     def disconnect(self, reason = None):
         dprint(self, "--", 0, "Disconnected" + " (%s)" % reason if reason else "")
         self.socket.shutdown(socket.SHUT_RDWR)
         self.socket.close()
-        for a in [read, write]:
-            if self in a: a.remove(self)
+        self.closed = True
 
 
-def process(c: Client, carousel):
+def process(c: Connection, carousel):
     if c.phase == 0:
         if len(c.read_buffer) >= 2:
             protocol_version = int(c.read_buffer[0])  # uint8, must be 0x05 for socks5
@@ -123,21 +125,11 @@ def process(c: Client, carousel):
                     c.append_write(form_response(SOCKS_STATUS_CONNECTION_NOT_ALLOWED, encoded_address))
                     dprint(c, "--", True, f"Invalid characters in address")
                 else:
-                    # Let's assume we're O.K!
-                    ssh_socket = carousel.setup(address.decode("ascii"), port)
-                    if not ssh_socket:
-                        c.append_write(form_response(SOCKS_STATUS_CONNECTION_NOT_ALLOWED,
-                                                     b"\x01\x00\x00\x00\x00" + struct.pack("!H", c.port)))
-                    else:
-                        ssh_client = Client(ssh_socket)
-                        ssh_client.phase = 3
-                        ssh_client.pair = c
-                        c.pair = ssh_client
-                        c.phase = 2
-                        read.append(ssh_client)
-                        c.append_write(form_response(SOCKS_STATUS_SUCCESS,
-                                                     b"\x01\x00\x00\x00\x00" + struct.pack("!H", c.port)))
-    elif c.phase in [2, 3]:
+                    c.requested_pair = (address.decode("ascii"), port)
+    elif c.phase == 2:
+        pass # Awaiting pickup this end
+
+    elif c.phase in [3, 4]:
         c.pair.append_write(c.read_buffer)
         c.read_buffer = b''
     else:
@@ -153,28 +145,26 @@ def server_socket(host, port, ip6=True):
     return s
 
 
-# TODO: not this
-read, write = [], []
-
 def mainloop(socks_host, socks_port, carousel):
-    global read, write
+    read, write = [], []
     server = server_socket(socks_host, socks_port)
     read = [server]
     write = []
 
     while True:
-        r, w, x = select.select(read, write, [], 2)
+        r, w, x = select.select(read, write, [], 1)
         ut_now = int(datetime.now().timestamp())
 
+        # Pick up new clients, process data
         for c in r:
             if c in [server]:
-                client = Client(server.accept(), False)
+                client = Connection(server.accept(), False)
                 dprint(client, "--", 1, "Connected")
                 read.append(client)
             else:
                 new_data = c.recv(1024)
                 if not len(new_data):
-                    dprint(c, "--", 1, f"Disconnected by client")
+                    dprint(c, "--", 1, f"Disconnected by remote end")
                     for connection_list in [read, write]:
                         if c in connection_list:
                             connection_list.remove(c)
@@ -182,17 +172,40 @@ def mainloop(socks_host, socks_port, carousel):
                     c.read_buffer += new_data
                     process(c, carousel)
 
-        for c in write:
+        # Handle a connection asking to be put through
+        for client in read:
+            if client == server:
+                continue
+            if client.requested_pair:
+                ssh_socket = carousel.setup(*client.requested_pair)
+                client.requested_pair = None
+                if not ssh_socket:
+                    client.append_write(form_response(SOCKS_STATUS_CONNECTION_NOT_ALLOWED,
+                                                 b"\x01\x00\x00\x00\x00" + struct.pack("!H", client.port)))
+                else:
+                    ssh_client = Connection(ssh_socket)
+                    ssh_client.phase = 3
+                    ssh_client.pair = client
+                    client.pair = ssh_client
+                    read.append(ssh_client)
+                    client.append_write(form_response(SOCKS_STATUS_SUCCESS,
+                                                 b"\x01\x00\x00\x00\x00" + struct.pack("!H", client.port)))
+                    client.phase = 4
+
+        # Write shit
+        for c in read:
+            if c == server:
+                continue
             if c.write_buffer:
                 c.write_buffer = c.write_buffer[c.send(c.write_buffer):]
-                if not c.write_buffer:
-                    write.remove(c)
 
-        for c in read + write:
-            if c in [server] or c.is_control:
+        for c in read:
+            if c == server or c.is_control:
                 continue
-            if ut_now - c.ut_accepted >= 10 and c.phase not in [2, 3]:
+            if ut_now - c.ut_accepted >= 10 and c.phase not in [3, 4]:
                 c.disconnect("Failed to complete SOCKS handshake in time")
+            if c.closed:
+                read.remove(c)
 
 
 def testloop(carousel):
@@ -203,7 +216,11 @@ if __name__ == "__main__":
     setup_logging()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--no-tor", "-N", action='store_true', default=False)
+    parser.add_argument("--no-tor", "-N", action='store_true', default=False, help="By default, tor is enabled. Use "
+                                                                                   "to disable")
+    parser.add_argument("--no-test", "-T", action='store_true', default=False, help="By default, if using -p,"
+                                                                                    "connections are tested in the"
+                                                                                    "background, this disables.")
 
     parser.add_argument("--socks-bind", type=str, default="::", help="socks5 bind host")
     parser.add_argument("--socks-host", type=int, default=8090, help="socks5 bind port")
